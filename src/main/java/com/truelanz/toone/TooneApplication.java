a7f953a67722a09fa2fa5ffd395e1ad0598e4252
truelanz <devalanoliveira@gmail.com>
2023-06-26 18:39:52 -0300

    first commit

---
+++ src/main/java/com/truelanz/toone/TooneApplication.java
@@ -0,0 +1,13 @@
+package com.truelanz.toone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TooneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truelanz/toone/TooneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truelanz.toone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
